83d4d487ef21c9a2dca7885c830aba99fa3ae066
javi171101 <javi.alonso2001@gmail.com>
2023-11-13 13:36:51 +0100

    Initial commit1
    Signed-off-by: javi171101 <javi.alonso2001@gmail.com>

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -3,6 +3,9 @@ package com.example.nicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+   //glide for loading girl
+        ImageView mGirl = findViewById(R.id.girl);
+
+
+        Glide.with(this)
+                .load(R.drawable.girl)
+//                .transition(DrawableTransitionOptions.withCrossFade(2000))
+//                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(mGirl);
+
+//        Glide.with(this)
+//                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+//                .into(mLogo);
+
+
     }
 }
