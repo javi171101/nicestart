12da0b81ba7b1f0ca5d832b1d733236bfd46e228
javi171101 <javi.alonso2001@gmail.com>
2023-11-15 13:07:54 +0100

    Initial commit1
    Signed-off-by: javi171101 <javi.alonso2001@gmail.com>

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -1,11 +1,21 @@
 package com.example.nicestart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.bumptech.glide.Glide;
+
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+
+
 
 public class Login extends AppCompatActivity {
 
@@ -13,9 +23,19 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-   //glide for loading girl
-        ImageView mGirl = findViewById(R.id.girl);
 
+
+        //implements and starts animation
+        // objeto thunder sobre el cual aplicaremos la animación
+        ImageView logo = findViewById(R.id.logo);
+
+
+
+
+
+//    Glide for loading girls
+        ImageView mGirl = findViewById(R.id.girl);
+        ImageView mLogo = findViewById(R.id.logo);
 
         Glide.with(this)
                 .load(R.drawable.girl)
@@ -30,6 +50,24 @@ public class Login extends AppCompatActivity {
 //                .circleCrop()
 //                .into(mLogo);
 
+    }
+
+
+
+
+    // Method for Login button
+    public void openMain(View v) {
+        Intent intent = new Intent(Login.this, Main.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
     }
+
+    // Method for Signup button
+    public void openSignup(View v) {
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
+    }
+
 }
