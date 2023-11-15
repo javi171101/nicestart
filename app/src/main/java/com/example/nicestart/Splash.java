fda307f52a685e4e098d117a548f3aef0614aba8
javi171101 <javi.alonso2001@gmail.com>
2023-11-15 12:46:45 +0100

    Initial commit1
    Signed-off-by: javi171101 <javi.alonso2001@gmail.com>

---
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -0,0 +1,60 @@
+package com.example.nicestart;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+openApp();
+
+
+
+        ImageView mSea = findViewById(R.id.backView);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+//                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mSea);
+
+    }
+
+    private void openApp() {
+        Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+
+
+            }
+        }, 5000);
+
+    }
+
+}
